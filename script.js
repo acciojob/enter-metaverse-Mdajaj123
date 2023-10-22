010789042ef3bea06f9b96c16a0da733e5fdc4ff
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-22 15:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removep(){
+	const p=document.getElementById("p");
+	const b=document.getElementById("btn")
+	const body=document.getElementsByTagName("body")[0];
+	p.remove();
+	const h=document.createElement("h1");
+	h.innerText="Entered Metaverse";
+	body.insertBefore(h,b);
+}
+
