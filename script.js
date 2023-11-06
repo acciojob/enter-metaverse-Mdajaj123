2d9b6246bf4601a5a151c4ac52d0d0ce66b28e68
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-11-07 03:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function removep(){
 	const p=document.querySelector("#status");
 	const b=document.getElementById("enterBtn")
 	const body=document.getElementsByTagName("body")[0];
-	body.remove(p);
-     // p.innerHTML='<h1>Entered Metaverse</h1>'
-	const h=document.createElement("h1");
-	h.innerText="Entered Metaverse";
-	body.insertBefore(h,b);
+	// body.remove(p);
+     p.innerHTML='<h1>Entered Metaverse</h1>'
+	// const h=document.createElement("h1");
+	// h.innerText="Entered Metaverse";
+	// body.insertBefore(h,b);
 	
 }
 
